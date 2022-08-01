dbf1c3ccb25320322ed9badaeca64d1526d9230f
yangshiqu <635408602@qq.com>
2022-08-01 21:55:39 +0800

    调整x86/64架构

--- libyuv-rs/src/planar_function.rs
+++ libyuv-rs/src/planar_function.rs
@@ -2,7 +2,7 @@ use crate::row_common::copy_row_rs;
 
 /// 复制数据平面
 pub fn copy_plane(src_y: &[u8], dst_y: &mut [u8]) {
-    #[cfg(any(target_arch = "x86_64", target_arch = "x86_64"))]
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     {
         if is_x86_feature_detected!("avx") {
             println!("go avx");
@@ -19,6 +19,20 @@ pub fn copy_plane(src_y: &[u8], dst_y: &mut [u8]) {
             unsafe { crate::row_gcc::copy_row_sse2(src_y, dst_y) };
             return ;
         }
+    }
+
+    #[cfg(all(target_arch = "aarch64"))]
+    {
+        println!("go neon");
+        // unsafe { crate::row_neon::copy_row_neon(src_y, dst_y) };
+        return ;
+    }
+
+    #[cfg(all(target_arch = "arm"))]
+    {
+        println!("go arm");
+        // unsafe { crate::row_neon::copy_row_neon(src_y, dst_y) };
+        return ;
     }
 
     {
